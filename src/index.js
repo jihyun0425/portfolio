efade13ae09645e58855d3fde952d30b02080c8a
jihyun <bjh2013@daum.net>
2024-03-29 14:53:24 +0900

    header commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./utils/App";
 import "./assets/scss/style.scss";
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './utils/reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
