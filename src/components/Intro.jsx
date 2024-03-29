53ea29cfa0082328c43bee0fd7d0192f75082f22
jihyun <bjh2013@daum.net>
2024-03-29 17:39:12 +0900

    site 수정

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -5,28 +5,28 @@ import about from "../assets/img/about.jpg";
 const Intro = () => {
     return (
         <section id="intro">
-            <div class="intro__inner">
-                <h1 class="intro__title">junior developer</h1>
-                <div class="intro__lines" aria-hidden="true">
-                    <span class="line"></span>
-                    <span class="line"></span>
-                    <span class="line"></span>
-                    <span class="line"></span>
-                    <span class="line"></span>
-                    <span class="line"></span>
-                    <span class="line"></span>
+            <div className="intro__inner">
+                <h1 className="intro__title">junior developer</h1>
+                <div className="intro__lines" aria-hidden="true">
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
                 </div>
-                    <div class="img">
+                    <div className="img">
                         <img src={about} alt="어바웃" />
                     </div>
                 <div class="intro__lines bottom" aria-hidden="true">
-                    <span class="line">이름</span>
-                    <span class="line">생년월일</span>
-                    <span class="line">주소</span>
-                    <span class="line">메일</span>
-                    <span class="line">학교</span>
-                    <span class="line">깃허브</span>
-                    <span class="line">블로그</span>
+                    <span className="line">이름</span>
+                    <span className="line">생년월일</span>
+                    <span className="line">주소</span>
+                    <span className="line">메일</span>
+                    <span className="line">학교</span>
+                    <span className="line">깃허브</span>
+                    <span className="line">블로그</span>
                 </div>
             </div>
         </section>
--- src/components/Site.jsx
+++ src/components/Site.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import githubBg from "../assets/img/githubBg.jpg"
+import blogBg from "../assets/img/blogBg.jpg"
 
 const Site = () => {
-    return <div>Site</div>;
+    return (
+        <section id="site">
+            <div className="site__inner">
+                <h2 className="site__title">Site</h2>
+                <div className="site__wrap">
+                            <h3 className="title">
+                               GitHub 주소
+                            </h3>
+                            <div className="link">
+                                <p>https://github.com/jihyun0425</p>
+                            </div>
+                            <div className="img">
+                                <a src={githubBg} alt="깃허브 사진"/>
+                            </div>
+                </div>
+                <div className="site__wrap">
+                            <h3 className="title">
+                               Blog 주소
+                            </h3>
+                            <div className="link">
+                                <p>https://blog.naver.com/jihyun9833</p>
+                            </div>
+                            <div className="img">
+                                <a src={blogBg} alt="블로그 사진"/>
+                            </div>
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default Site;
