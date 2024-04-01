241c26888f822d720199362cae8a4ddbc325d5dc
jihyun <bjh2013@daum.net>
2024-04-01 16:30:42 +0900

    project commit

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import proText from "../constants/projects"
 
 const Projects = () => {
-    return <div>Port</div>;
+    return (
+        <section id="project">
+            <div className="pro__inner">
+                <div className="pro__title">
+                    Projects
+                </div>
+                <div className="pro__wrap">
+                    {proText.map((pro, key) => (
+                        <article className={`pro__item p${key + 1}`} key={key}>
+                            <span className="num">{pro.num}.</span>
+                            <a href={pro.code} target="_blank" className="img" rel="noreferrer">
+                                <img src={pro.img} alt={pro.name} />
+                            </a>
+                            <h3 className="title">{pro.title}</h3>
+                            <p className="desc">{pro.desc}</p>
+                            <a href={pro.view} target="_blank" className="site" rel="noreferrer">사이트 보기</a>
+                        </article>
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default Projects;
---
+++ src/constants/projects.jsx
@@ -0,0 +1,55 @@
+import port01 from "../assets/img/port01.jpg";
+import port02 from "../assets/img/port02.jpg";
+import port03 from "../assets/img/port03.jpg";
+import port04 from "../assets/img/port04.jpg";
+import port05 from "../assets/img/port05.jpg";
+
+const proText = [
+    {
+        num: "01",
+        title: "easy-diary",
+        desc: "다양한 템플릿을 지원하는 일기 사이트입니다",
+        img: port01,
+        code: "https://github.com/jihyun0425/easy-diary",
+        view: "#",
+        name: "easy-diary",
+    },
+    {
+        num: "02",
+        title: "night-factory",
+        desc: "방탈출 테마 소개 및 예약을 지원하는 사이트입니다",
+        img: port02,
+        code: "https://github.com/jihyun0425/night-factory",
+        view: "#",
+        name: "night-factory",
+    },
+    {
+        num: "03",
+        title: "minimal-lounge",
+        desc: "작은 소품들을 판매하는 쇼핑몰 사이트입니다",
+        img: port03,
+        code: "https://github.com/jihyun0425/minimal-lounge",
+        view: "#",
+        name: "minimal-lounge",
+    },
+    {
+        num: "04",
+        title: "android",
+        desc: "사람의 기본정보를 입력하고 저장, 수정, 삭제가 가능한 어플리케이션입니다",
+        img: port04,
+        code: "https://github.com/jihyun0425/android",
+        view: "#",
+        name: "android",
+    },
+    {
+        num: "05",
+        title: "portfolio",
+        desc: "리액트를 이용하여 제작한 포트폴리오입니다",
+        img: port05,
+        code: "https://github.com/jihyun0425/portfolio",
+        view: "#",
+        name: "portfolio",
+    },
+];
+
+export default proText;
