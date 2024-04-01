665e8eaefe1408d8dde57257adf7a48c11a8d613
jihyun <bjh2013@daum.net>
2024-04-01 16:46:40 +0900

    contact commit

---
+++ src/constants/contact.jsx
@@ -0,0 +1,12 @@
+const contactText = [
+    {
+        link: "https://open.kakao.com/me/jihyun0425",
+        title: "KAKAO : Jihyun",
+    },
+    {
+        link: "bjh8332@gmail.com",
+        title: "mail : bjh8332@gmail.com",
+    },
+];
+
+export default contactText;
